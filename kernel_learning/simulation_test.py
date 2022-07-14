d5927cd7d42933690c65cba34a3eb5e12174db14
Allen Ross <allenr15@vt.edu>
2022-07-13 22:42:01 -0400

    Tweak simulation parameters slightly to be "longitudinal"

--- kernel_learning/simulation_test.py
+++ kernel_learning/simulation_test.py
@@ -184,10 +184,10 @@ if __name__ == "__main__":
 
     # Set options
     np.random.seed(9102+task_id)
-    rates = [4, 8, 16, 32] #[5, 10, 20, 50] # [3, 9] #[2, 4, 12]
-    units = [20, 40, 60, 80]
+    rates = [3, 6, 12, 24] #[5, 10, 20, 50] # [3, 9] #[2, 4, 12]
+    units = [30, 60, 120, 240]
     epsilons = [0, 0.3, 3.0, 30] # SNR [\inf, 10, 1, 0.1]
-    iters = 10
+    iters = 3
     sim_settings = list(itertools.product(*[rates, epsilons, units, list(range(0, iters))]))
     np.random.shuffle(sim_settings)
     #print(sim_settings)
