606464a6cd00f7b9b758183e8c42883cc6d950aa
Allen Ross <allenr15@vt.edu>
2024-04-14 21:54:58 -0600

    Clean up setup with new dependencies and update readme for python version

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ This repository houses code for the _Waveome_ package - an easy to use and power
 Please see the `waveome_overview.ipynb` for an example of the package modeling simulated data.
 
 ## Installation
-To install the package it is suggested that you create a new conda environment   
-`conda create -n waveome python=3.8.12`   
+To install the package it is suggested that you create a new conda environment (required to have Python >= 3.9 and <= 3.11 for tensorflow)   
+`conda create -n waveome python=3.11`   
 `conda activate waveome`   
 and then pip install _waveome_ using    
 `pip install git+https://github.com/omicsEye/waveome.git`.
@@ -21,4 +21,4 @@ and then pip install _waveome_ using
 Warning: If you are using an M1 Mac then before `pip install` you should run `conda install -c conda-forge grpcio`!
 
 ## Jupyter Notebook
-If you would like to run `waveome_overview.ipynb` then you should also set up a Jupyter kernel for the new waveome environment. This can be done with `conda install ipykernel` and then `python -m ipykernel install --user --name=waveome`.
+If you would like to run `waveome_overview.ipynb` then you should also set up a Jupyter kernel for the new waveome environment. This can be done with `conda install -n waveome ipykernel` and then `python -m ipykernel install --user --name=waveome`.
--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
 import setuptools
 
 setuptools.setup(
-    name='waveome',
-    version='0.0.2',
-    author='Allen Ross',
-    author_email='allenross@gwu.edu',
-    description='Automated longitduinal data analysis using Gaussian processes',
-    url='https://github.com/omicsEye/waveome',
+    name="waveome",
+    version="0.0.2",
+    author="Allen Ross",
+    author_email="allenross@gwu.edu",
+    description="""
+        Automated longitudinal data analysis using Gaussian processes.
+    """,
+    url="https://github.com/omicsEye/waveome",
     project_urls={
         "Bug Tracker": "https://github.com/omicsEye/waveome/issues"
     },
-    license='MIT',
-    packages=['waveome'],
+    license="MIT",
+    packages=["waveome"],
     install_requires=[
-        'gpflow==2.9.0',
-        'tensorflow_probability==0.21.0',
-        'tensorflow==2.13.0',
-        # "tensorflow-macos; platform_machine=='arm64'",
-        'pandas',
-        'numpy',
-        'scipy',
-        'joblib',
-        'seaborn',
-        'matplotlib',
-        'tqdm'
+        # Main package - doesn't handle TF well at install
+        "gpflow==2.9.1",
+        # Need to pin TF and TF prob together
+        "tensorflow_probability==0.23.0",
+        "tensorflow==2.15.1",
+        "pandas",
+        "numpy",
+        "scipy",
+        "joblib",
+        "seaborn",
+        "matplotlib",
+        "tqdm"
     ],
 )
